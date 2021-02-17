10a8c893122f170a86fbce461b7eb733597fe960
shahrukh-prog <79189810+shahrukh-prog@users.noreply.github.com>
2021-02-17 12:25:52 +0500

    Add files via upload

---
+++ 16 FEB 2021.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+using namespace std;
+
+/* QUESTION NUMBER 1:
+ float convert(float km, float mile)
+{
+   return	km * 1.609;
+}
+int main()
+{
+	float km, mile;
+	cout<<"Enter value in km: ";
+	cin>>km;
+		cout<<"Value in mile is: ";
+		cout<<convert(km,mile) ; //Function Call
+	
+ 
+}*/
+
+/* QUESTION NUMBER 2:
+float months(float year, float month)
+{
+	
+return year*12;
+}
+float day(float year, float days)
+
+{
+	return year*365;
+}
+int main()
+{
+float year, month ,days;
+ cout<<"Enter age in year: ";
+ cin>>year;
+ cout<<"Age in months is: ";
+ cout<<months(year,month)<<endl;
+ cout<<"Age in days is: ";
+ cout<<day(year,days);
+} */
+
+/* QUESTION NUMBER 3:
+float square(float num, float sq)
+{
+	return num*num;
+}
+float cube(float num,float cb)
+{
+	return num*num*num;
+}
+int main()
+{
+	float num,sq,cb;
+	cout<<"Enter the number: ";
+	cin>>num;
+	cout<<"Square of number is: ";
+	cout<<square(num,sq)<<endl;
+	cout<<"Cube of number is: ";
+	cout<<cube(num,cb);
+}*/
+/*QUESTION NUMBER 4:
+float RPG(int days, int PPG) //RPG: read pages, PPG pages per day
+{
+return  days*PPG;
+}
+float LPG(int TPG,int RPGs) //LPG:left pages, TPG: total pages
+{
+	return TPG-RPGs;
+}
+int main()
+{
+	int TPG,RPGs,PPG,days;
+	cout<<"Enter total pages: ";
+	cin>>TPG;
+	cout<<"Pages per day: ";
+	cin>>PPG;
+	cout<<"Number of days: ";
+	cin>>days;
+	cout<<"Number of pages read are: ";
+	cout<<RPG(days,PPG)<<endl;
+	RPGs=RPG(days,PPG);
+	cout<<"Number of pages left are: ";
+	cout<<LPG(TPG,RPGs);
+}*/
+
+/*QUESTION NUMBER 5:
+float distance(float petrol,float mileage)
+{
+	return petrol*mileage;
+}
+int maim()
+{
+float petrol,mileage;
+cout<<"Enter petrol: ";
+cin>>petrol;
+mileage==5.3;
+cout<<"Distance car can travel is: ";
+cout<<distance(petrol,mileage);
+}*/
+
